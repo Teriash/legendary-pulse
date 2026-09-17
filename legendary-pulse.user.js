4053c3ad19576710d361b98b70a1f9db26cdf1bb
Teriash <138162965+Teriash@users.noreply.github.com>
2026-09-17 02:02:14 +0200

    Update legendary-pulse.user.js

--- legendary-pulse.user.js
+++ legendary-pulse.user.js
@@ -2,7 +2,7 @@
 // @name         Legendary Pulse
 // @namespace    https://margonem.pl/
 // @namespace    https://margonem.com/
-// @version      1.10.
+// @version      1.10.0
 // @description  Notyfikator legend
 // @author       Teriash
 // @updateURL    https://github.com/Teriash/legendary-pulse/raw/refs/heads/main/legendary-pulse.user.js
